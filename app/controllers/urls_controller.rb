e97dfa6895c92db199661b1efd4c88fcaed7687c
jongmini <jongmin.choi@gmail.com>
2014-01-25 17:04:32 -0800

    basic function working

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -13,15 +13,38 @@ class UrlsController < ApplicationController
 
   end
 
-  def edit
-  end
-
   def create
   	url = params.require(:url).permit(:og_url, :new_url)
-  	new_url = url["og_url"]
-		random_string=SecureRandom.urlsafe_base64(new_url.length)
-		@url = Url.create(og_url: new_url, new_url: random_string)
+  	user_url = url["og_url"] # www.addy.com
+    # section that checks if the user input string exists in the db
+    new_string = url["new_url"] # input_string
+      
+      if new_string == ""
+        binding.pry
+        string = SecureRandom.urlsafe_base64(user_url.length)
+      elsif Url.where(new_url: new_string).exists?
+        binding.pry
+        string = SecureRandom.urlsafe_base64(user_url.length)
+      else
+        string = new_string
+      end
+  
+
+		# random_string=SecureRandom.urlsafe_base64(user_url.length)
+		@url = Url.create(og_url: user_url, new_url: string)
 		redirect_to "/go/#{@url.new_url}/preview"
+  end
+
+  def visit
+    string = params[:string]
+    user_url = Url.find_by new_url: string
+
+    redirect_to "http://#{user_url.og_url}"
+  end
+
+
+
+  def edit
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,18 @@ Ritly::Application.routes.draw do
 
   get '/', to: 'urls#index', as: :root
 
-  get '/go/:string/preview', to: 'urls#show', as: :show
+  get '/go/:string', to: 'urls#visit', as: :visit
 
-  # get 'go/:string/edit', to: 'urls#edit', as: :edit_url
+  get '/go/:string/preview', to: 'urls#show', as: :show
 
   post '/', to: 'urls#create'
 
-  # patch '/go/:string', to: 'urls#update'
-
-
   
 end
+
+
+
+
+
+ # get 'go/:string/edit', to: 'urls#edit', as: :edit_url
+  # patch '/go/:string', to: 'urls#update'
